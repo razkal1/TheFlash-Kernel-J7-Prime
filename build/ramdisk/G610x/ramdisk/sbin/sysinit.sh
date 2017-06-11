f5013a9a83d7311499f99e5e466d92b1e9b8a11a
Siddhant-Naik <Siddhant-Naik@github.com>
2017-06-11 19:11:07 +0530

    Import ramdisk from my Lazer Kernel(AQC4) -Fake knox fixed -Removed supersu from ramdisk -Some tweaks may be lost. I'll add them soon -Added placeholders

--- build/ramdisk/G610x/ramdisk/sbin/sysinit.sh
+++ build/ramdisk/G610x/ramdisk/sbin/sysinit.sh
@@ -1,74 +1,29 @@
 #!/system/bin/sh
-
-# Busybox
-if [ -e /su/xbin/busybox ]; then
-	BB=/su/xbin/busybox;
-else if [ -e /sbin/busybox ]; then
-	BB=/sbin/busybox;
-else
-	BB=/system/xbin/busybox;
-fi;
-fi;
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 mount -o remount,rw /;
 mount -o rw,remount /system
 
-# Knox set to 0 on working system
-/sbin/resetprop -n ro.boot.warranty_bit "0"
-/sbin/resetprop -n ro.warranty_bit "0"
-
-# Fix some safetynet flags
-/sbin/resetprop -n ro.boot.veritymode "enforcing"
-/sbin/resetprop -n ro.boot.verifiedbootstate "green"
-/sbin/resetprop -n ro.boot.flash.locked "1"
-/sbin/resetprop -n ro.boot.ddrinfo "00000001"
-
-#-------------------------
-# TWEAKS
-#-------------------------
-
-    # SD-Card Readhead
-    echo "2048" > /sys/devices/virtual/bdi/179:0/read_ahead_kb;
-
-    # Internet Speed
-    echo "0" > /proc/sys/net/ipv4/tcp_timestamps;
-    echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse;
-    echo "1" > /proc/sys/net/ipv4/tcp_sack;
-    echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle;
-    echo "1" > /proc/sys/net/ipv4/tcp_window_scaling;
-    echo "5" > /proc/sys/net/ipv4/tcp_keepalive_probes;
-    echo "30" > /proc/sys/net/ipv4/tcp_keepalive_intvl;
-    echo "30" > /proc/sys/net/ipv4/tcp_fin_timeout;
-    echo "404480" > /proc/sys/net/core/wmem_max;
-    echo "404480" > /proc/sys/net/core/rmem_max;
-    echo "256960" > /proc/sys/net/core/rmem_default;
-    echo "256960" > /proc/sys/net/core/wmem_default;
-    echo "4096,16384,404480" > /proc/sys/net/ipv4/tcp_wmem;
-    echo "4096,87380,404480" > /proc/sys/net/ipv4/tcp_rmem;
-
-#-------------------------
-# KERNEL INIT VALUES
-#-------------------------
-
-    # Enable Dynamic Fsync
-    echo "1" > /sys/kernel/dyn_fsync/Dyn_fsync_active
-
-    # Enable Powersuspend
-    echo "1" > /sys/kernel/dyn_fsync/Dyn_fsync_earlysuspend
-	
-# mediakill (Credits: lyapota)
-killall -9 android.process.media
-killall -9 mediaserver
-
 # init.d support
 if [ ! -e /system/etc/init.d ]; then
-   mkdir /system/etc/init.d
-   chown -R root.root /system/etc/init.d
-   chmod -R 755 /system/etc/init.d
+	mkdir /system/etc/init.d
+	chown -R root.root /system/etc/init.d
+	chmod -R 755 /system/etc/init.d
 fi
 
 # start init.d
 for FILE in /system/etc/init.d/*; do
-   sh $FILE >/dev/null
+	sh $FILE >/dev/null
 done;
-
