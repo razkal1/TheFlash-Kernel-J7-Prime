84b60eb7ef683c18c4dec7ea57a19b8dfc44848b
Siddhant-Naik <Siddhant-Naik@github.com>
2017-06-11 23:49:31 +0530

    Linux:3.18.32

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5336,6 +5336,20 @@ static void lockdep_set_quota_inode(struct inode *inode, int subclass)
 	lockdep_set_subclass(&ei->i_data_sem, subclass);
 }
 
+static void lockdep_set_quota_inode(struct inode *inode, int subclass)
+{
+	struct ext4_inode_info *ei = EXT4_I(inode);
+
+	/* The first argument of lockdep_set_subclass has to be
+	 * *exactly* the same as the argument to init_rwsem() --- in
+	 * this case, in init_once() --- or lockdep gets unhappy
+	 * because the name of the lock is set using the
+	 * stringification of the argument to init_rwsem().
+	 */
+	(void) ei;	/* shut up clang warning if !CONFIG_LOCKDEP */
+	lockdep_set_subclass(&ei->i_data_sem, subclass);
+}
+
 /*
  * Standard function to be called on quota_on
  */
