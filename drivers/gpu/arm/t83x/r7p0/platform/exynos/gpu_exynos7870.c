c824d3d80d664b1e7607bfd7226453d2efd195cd
Siddhant-Naik <Siddhant-Naik@github.com>
2017-07-01 21:26:21 +0530

    OC gpu to 1246MHz

--- drivers/gpu/arm/t83x/r7p0/platform/exynos/gpu_exynos7870.c
+++ drivers/gpu/arm/t83x/r7p0/platform/exynos/gpu_exynos7870.c
@@ -74,8 +74,8 @@ void __iomem *g3d1_outstanding_regs;
 /*  clk,vol,abb,min,max,down stay, pm_qos mem, pm_qos int, pm_qos cpu_kfc_min, pm_qos cpu_egl_max */
 static gpu_dvfs_info gpu_dvfs_table_default[] = {
 	{1300, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1246, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
-	{1146, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1246, 950000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
+	{1146, 950000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
 	{1001, 900000, 0, 98, 100, 1, 0, 902000, 400000, 1586000, CPU_MAX},
 	{845,  900000, 0, 78,  98, 1, 0, 836000, 400000, 1248000, CPU_MAX},
 	{728,  900000, 0, 78,  85, 1, 0, 728000, 400000, 1144000, CPU_MAX},
@@ -94,8 +94,8 @@ static int mif_min_table[] = {
 };
 
 static gpu_attribute gpu_config_attributes[] = {
-	{GPU_MAX_CLOCK, 1001},
-	{GPU_MAX_CLOCK_LIMIT, 1001},
+	{GPU_MAX_CLOCK, 1246},
+	{GPU_MAX_CLOCK_LIMIT, 1246},
 	{GPU_MIN_CLOCK, 343},
 	{GPU_DVFS_START_CLOCK, 343},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 343},
